c774802f76739e3f01832b4fbf81865f7b013bf4
j4l13n <juliushirwa@gmail.com>
2018-11-14 08:27:08 -0400

    working on stock management dashboard to get the last value

--- instance/config.py
+++ instance/config.py
@@ -7,7 +7,7 @@ TOP_LEVEL_DIR = os.path.abspath(os.curdir)
 
 
 SECRET_KEY = "thisisthesecretkeyhere"
-SQLALCHEMY_DATABASE_URI  = "mysql://root:regedit56mysql@localhost/coop"
+SQLALCHEMY_DATABASE_URI  = "mysql://root:annemuhiza@localhost/coop"
 
 
 
