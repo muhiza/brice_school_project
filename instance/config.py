00504e462e3da1c491dd168e109666a0663965b1
muhiza <muhizafrank@gmail.com>
2020-04-02 23:43:05 +0200

    Changed db database to meet production password

--- instance/config.py
+++ instance/config.py
@@ -6,7 +6,7 @@
 """
 import os
 
-SQLALCHEMY_DATABASE_URI  = "mysql://root:Copa123!#@localhost/aicos"
+
 
 # Accessing the current directory that we are working with
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
@@ -14,7 +14,7 @@ TOP_LEVEL_DIR = os.path.abspath(os.curdir)
 
 # These are the environment variables that are used for securing the application.
 SECRET_KEY = "thisisthesecretkeyhere"
-SQLALCHEMY_DATABASE_URI  = "mysql://muhiza:annemuhiza@localhost/coop"
+SQLALCHEMY_DATABASE_URI  = "mysql://root:Copa123!#@localhost/aicos"
 RECAPTCHA_PUBLIC_KEY = '6LdYIDcUAAAAAEE3N3tNqcYu50MJSTlGA5lwu5Pl'
 RECAPTCHA_PRIVATE_KEY = '6LdYIDcUAAAAAD8ayN_2Mkhauh_-MdK12XxdTLEo'
 
@@ -24,5 +24,19 @@ UPLOADS_DEFAULT_DEST = TOP_LEVEL_DIR + '/project/static/img/'
 UPLOADS_DEFAULT_URL = 'http://localhost:5000/static/img/'
 UPLOADED_IMAGES_DEST = TOP_LEVEL_DIR + '/project/static/img/'
 UPLOADED_IMAGES_URL = 'http://localhost:5000/static/img/'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
